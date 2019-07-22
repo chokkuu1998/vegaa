f1f8b0a238255fd6ab4289c8970a46e97e845af7
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-22 15:32:12 +0530

    Update 45.py

--- 45.py
+++ 45.py
@@ -1,7 +1,7 @@
-angle=int(input())
-b=math.radians(angle)
-y=math.sin(b)
-if(b<1):
-    print(round(y,1))
-elif(b>1):
-    print(round(y))
+anglee=int(input())
+bb=math.radians(anglee)
+yy=math.sin(bb)
+if(bb<1):
+    print(round(yy,1))
+elif(bb>1):
+    print(round(yy))
