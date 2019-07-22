f2e96de090c0ff851563b55b836ea6c206f73e0f
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-22 12:35:38 +0530

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,22 @@
+ss=input()
+
+
+b=[]
+
+for i in ss:
+
+    if(i>='A' and i<='V'):
+    
+        d=chr(ord(i)+3)
+        
+        b.append(d)
+        
+    else:
+    
+        c=chr(ord(i)-23)
+        
+        b.append(c)   
+        
+for i in b:
+
+    print(i,end="")
